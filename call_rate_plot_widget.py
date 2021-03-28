86fdef57ab4c953056c2ac32d986411e841648ef
Christopher Sund <csunday95@gmail.com>
2021-03-28 09:57:02 -0700

    added rng ticks total plot

--- call_rate_plot_widget.py
+++ call_rate_plot_widget.py
@@ -12,15 +12,26 @@ class CallRatePlotWidget(QWidget):
         self._main_layout = QVBoxLayout()
         self._rate_plot_widget = PlotWidget()
         self._rate_plot_item = self._rate_plot_widget.getPlotItem()
-        self._rate_plot = self._rate_plot_item.plot()
+        self._rate_plot_item.setTitle('RNG Call Rate')
+        self._rate_plot_item.setLabels(left='Call Rate [Calls/sec]', bottom='Time [s]')
+        self._rate_plot = self._rate_plot_item.plot(antialias=False)
         self._main_layout.addWidget(self._rate_plot_widget)
-        self._buffer = CircularFloatBuffer(2, maxlen=10000)
+        self._total_plot_widget = PlotWidget()
+        self._total_plot_item = self._total_plot_widget.getPlotItem()
+        self._total_plot_item.setTitle('Total RNG Calls')
+        self._total_plot_item.setLabels(left='Total RNG Calls', bottom='Time [s]')
+        self._total_plot = self._total_plot_item.plot()
+        self._main_layout.addWidget(self._total_plot_widget)
+        self._buffer = CircularFloatBuffer(3, maxlen=10000)
         self._start_time = time.perf_counter()
         self.setLayout(self._main_layout)
 
     def append_new_call_data(self, steps_taken, last_second_avg, total_steps):
         time_elapsed = time.perf_counter() - self._start_time
-        self._buffer.append([time_elapsed, last_second_avg])
+        self._buffer.append([time_elapsed, last_second_avg, total_steps])
         self._rate_plot.setData(
             self._buffer.get_all_for_channel(0), self._buffer.get_all_for_channel(1)
+        )
+        self._total_plot.setData(
+            self._buffer.get_all_for_channel(0), self._buffer.get_all_for_channel(2)
         )
